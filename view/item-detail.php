db85ea4690a17eefd0c07fc8412082661fa1adcf
Unknown <moriahmaney@gmail.com>
2017-03-18 22:48:31 -0400

    permanent fix to push footer down

--- view/item-detail.php
+++ view/item-detail.php
@@ -16,110 +16,68 @@ if(isset($_GET['id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title><?php echo $item->title?></title>
+    <title>Sticker Shock</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/shop-item.css" rel="stylesheet">
-
 </head>
 
 <body>
 
-    <?php include 'navigation.php'; ?>
+<?php include "../view/navigation.php";?>
 
-    <div class="container" style="margin-top: 1rem;">
+<div class="container" style="margin-top: 1rem;">
 
-        <div class="row">
+    <div class="row">
 
-            <?php include 'category-menu.php'; ?>
+        <?php include 'category-menu.php'; ?>
 
-            <div class="col-md-9">
+        <div class="col-md-9">
 
-                <div class="thumbnail">
-                    <img class="img-responsive" src="http://placehold.it/800x300" alt="">
-                    <div class="caption-full">
-                        <h4><?php echo $item->title?></h4>
-                        <h5 class="pull-right">$<?php echo $item->price?></h5>
-                        <p><?php echo $item->description?></p>
-                    </div>
-
-                    <div class="ratings">
-                        <p class="pull-right">3 reviews</p>
-                        <p>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star-empty"></span>
-                            4.0 stars
-                        </p>
-                    </div>
+            <div class="thumbnail">
+                <img class="img-responsive" src="http://placehold.it/800x300" alt="">
+                <div class="caption-full">
+                    <h4><?php echo $item->title?></h4>
+                    <h5 class="pull-right">$<?php echo $item->price?></h5>
+                    <p><?php echo $item->description?></p>
                 </div>
 
-                <div class="well">
+                <div class="ratings">
+                    <p class="pull-right">3 reviews</p>
+                    <p>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        4.0 stars
+                    </p>
+                </div>
+            </div>
 
-                    <div class="text-right">
-                        <a class="btn btn-success">Leave a Review</a>
-                    </div>
+            <div class="well">
 
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star-empty"></span>
-                            Anonymous
-                            <span class="pull-right">10 days ago</span>
-                            <p>This product was great in terms of quality. I would definitely buy another!</p>
-                        </div>
-                    </div>
-
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star-empty"></span>
-                            Anonymous
-                            <span class="pull-right">12 days ago</span>
-                            <p>I've already ordered another one!</p>
-                        </div>
-                    </div>
-
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star"></span>
-                            <span class="glyphicon glyphicon-star-empty"></span>
-                            Anonymous
-                            <span class="pull-right">15 days ago</span>
-                            <p>I've seen some better than this, but not at this price. I definitely recommend this item.</p>
-                        </div>
-                    </div>
-
+                <div class="text-right">
+                    <a class="btn btn-success">Leave a Review</a>
                 </div>
 
+                <hr>
+
+                <?php include "review.php"?>
             </div>
 
         </div>
 
     </div>
 
+</div>
 
-    <?php include 'footer.php'; ?>
 
-    <script src="../js/jquery.js"></script>
-    <script src="../js/tether.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
+<?php include '../view/footer.php';?>
+
+<script src="../js/jquery.js"></script>
+<script src="../js/tether.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
---
+++ view/review.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-md-12">
+        <span class="glyphicon glyphicon-star"></span>
+        <span class="glyphicon glyphicon-star"></span>
+        <span class="glyphicon glyphicon-star"></span>
+        <span class="glyphicon glyphicon-star"></span>
+        <span class="glyphicon glyphicon-star-empty"></span>
+        Anonymous
+        <span class="pull-right">15 days ago</span>
+        <p>I've seen some better than this, but not at this price. I definitely recommend this item.</p>
+    </div>
+</div>
