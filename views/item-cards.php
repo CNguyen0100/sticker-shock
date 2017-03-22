0e308c74504b64601ed7c86f681f0189535bc844
Unknown <moriahmaney@gmail.com>
2017-03-21 21:41:17 -0400

    changed item_cards to use item_name instead of title

--- views/item-cards.php
+++ views/item-cards.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(E_ALL);
+// I don't know if you need to wrap the 1 inside of double quotes.
+ini_set("display_startup_errors",1);
+ini_set("display_errors",1);
 include "./models/Item.php";
 $items = Item::allItems();
 foreach($items as $item)  :?>
@@ -6,7 +10,7 @@ foreach($items as $item)  :?>
     <div class="card h-100">
         <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
         <div class="card-block">
-            <h4 class="card-title"><a href="views/item-detail.php?id=<?php echo $item->item_id?>"><?php echo $item->title?></a></h4>
+            <h4 class="card-title"><a href="views/item-detail.php?id=<?php echo $item->item_id?>"><?php echo $item->item_name?></a></h4>
             <h5>$<?php echo $item->price?></h5>
             <p class="card-text"><?php echo $item->description?></p>
         </div>
