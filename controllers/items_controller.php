b71c76ed605efba038fa4d7188dc504e5bc87e48
root <root@ubuntu-512mb-nyc3-01.localdomain>
2017-03-24 16:08:53 +0000

    Renamed Pages_controller.php to pages_controller.php for case consistency in URLs.

---
+++ controllers/items_controller.php
@@ -0,0 +1,32 @@
+<?php
+
+class Items extends Controller {
+    
+    public $id = null;
+
+    public function index() {
+        $this->title="Browse";
+        $items = $this->model->getAllItems();
+
+        require './views/layouts/header.php';
+        require './views/items/index.php';
+        require './views/layouts/footer.php';
+    }
+
+    public function item($id) {
+        $item = $this->model->getItemById($id);
+
+        $title = $item->item_name;
+
+        require './views/layouts/header.php';
+        require './views/items/item.php';
+        require './views/layouts/footer.php';
+    }
+
+    public function loadModel()
+    {
+        require './models/Item.php';
+        $this->model = new Item($this->db);
+    }
+
+}
