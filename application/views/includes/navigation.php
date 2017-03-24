87ce14b07125038ac8857bc7cc2e0ee14237a125
root <root@ubuntu-512mb-nyc3-01.localdomain>
2017-03-24 20:11:46 +0000

    Updated Account link to use account controller.

--- application/views/includes/navigation.php
+++ application/views/includes/navigation.php
@@ -13,7 +13,7 @@
                     <a class="nav-link" href="#">Sell</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Account</a>
+                    <a class="nav-link" href="account">Account</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="pages/contact">Contact Us</a>
