a88dec0cabc4cb6f33238f02339cd062291fdc5e
root <root@ubuntu-512mb-nyc3-01.localdomain>
2017-03-24 16:20:23 +0000

    Updated links to invokes the items controller.

--- views/item-cards.php
+++ views/item-cards.php
@@ -4,7 +4,7 @@ foreach($items as $item)  :?>
     <div class="card h-100">
         <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
         <div class="card-block">
-            <h4 class="card-title"><a href="views/item-detail.php?id=<?php echo $item->item_id?>"><?php echo $item->item_name?></a></h4>
+            <h4 class="card-title"><a href="./items/item/<?php echo $item->item_id?>"><?php echo $item->item_name?></a></h4>
             <h5>$<?php echo $item->price?></h5>
             <p class="card-text"><?php echo $item->description?></p>
         </div>
