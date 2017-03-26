de2d4d61206bcac6dbce4c13756a19913a136f76
root <root@ubuntu-512mb-nyc3-01.localdomain>
2017-03-26 00:44:15 +0000

    Header and footer partial views are now required in the views instead of the controllers.

--- application/views/pages/contact-us.php
+++ application/views/pages/contact-us.php
@@ -1,3 +1,5 @@
-<div class="container">
-    <p>Form here.</p>
-</div>
+<?php require 'application/views/layouts/header.php'; ?>
+    <div class="container">
+        <p>Form here.</p>
+    </div>
+<?php require 'application/views/layouts/footer.php'; ?>
--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,10 +1,12 @@
-<div class="container">
-    <div class="row">
-        <?php include 'application/views/includes/category-menu.php';?>
-        <div class="col-md-9">
-            <div class="row">
-                <?php include 'application/views/includes/item-cards.php';?>
+<?php require 'application/views/layouts/header.php'; ?>
+    <div class="container">
+        <div class="row">
+            <?php include 'application/views/includes/category-menu.php';?>
+            <div class="col-md-9">
+                <div class="row">
+                    <?php include 'application/views/includes/item-cards.php';?>
+                </div>
             </div>
         </div>
     </div>
-</div>
+<?php require 'application/views/layouts/footer.php'; ?>
