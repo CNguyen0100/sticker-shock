d9b7d48812898d5bb1fc364bd82d2e83ffd82255
root <root@ubuntu-512mb-nyc3-01.localdomain>
2017-03-28 21:34:04 +0000

    Added support for categories, and subcategories.

--- application/controllers/items_controller.php
+++ application/controllers/items_controller.php
@@ -1,12 +1,42 @@
 <?php
+require 'application/controllers/helpers/categories.php';
+require 'application/controllers/helpers/subcategories.php';
 
 class Items extends Controller {
     
     public $id = null;
+    public $category = "";
+    public $subcategory = "";
 
-    public function index() {
-        $this->title="Browse";
-        $items = $this->model->getAllItems();
+    public function index($category, $args) {
+
+        if (isset($category)) {
+            if (! $this->category = Category::isValid($category)) {
+                header('location: ' . URL . 'pages/error');
+                return;
+            }
+
+            if (isset($args[0])) {
+                if (! $this->subcategory = Subcategory::isValid($args[0])) {
+                    header('location: ' . URL . 'pages/error');
+                    return;
+                }
+
+                $this->title = ucwords("$this->category - $this->subcategory");
+                
+                if ($this->subcategory == 'tanktops') {
+                    $this->subcategory = "tank tops";
+                }
+
+                $items = $this->model->getItemsBySubcategory($this->category, $this->subcategory);
+            } else {
+                $this->title = ucfirst("$this->category");
+                $items = $this->model->getItemsByCategory($this->category);
+            }
+        } else {
+            $this->title="Browse";
+            $items = $this->model->getAllItems();
+        }
 
         require 'application/views/items/index.php';
     }
--- application/views/includes/item-cards.php
+++ application/views/includes/item-cards.php
@@ -1,15 +1,16 @@
 <?php
+#echo "$category" . ' ' . "$subcategory";
 foreach($items as $item)  :?>
 <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
         <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
         <div class="card-block">
+            <small><?php echo $item->category; if (isset($item->subcategory)) { echo ' / ' . $item->subcategory; } ?></small>
             <h4 class="card-title"><a href="./items/item/<?php echo $item->item_id?>"><?php echo $item->item_name?></a></h4>
             <h5>$<?php echo $item->price?></h5>
             <p class="card-text"><?php echo $item->description?></p>
         </div>
         <div class="card-footer">
-            <!-- <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small> -->
             <big>&#9733; &#9733; &#9733; &#9733; &#9734;</big>
         </div>
     </div>
