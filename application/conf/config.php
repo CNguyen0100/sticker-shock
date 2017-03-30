3a6370f2b62146338b215c8ce387bc1d5bbe595d
Graham L <grahamlup@gmail.com>
2017-03-30 18:24:27 +0000

    Removed unused/uncessary code. Added comments.

--- application/conf/config.php
+++ application/conf/config.php
@@ -1,17 +1,14 @@
 <?php
 
+# Graham L.:
+# Error reporting would obviously be disabled if this project were ever
+# in an actual production setting but we probably don't need to touch it.
 define('ENVIRONMENT', 'development');
 
 if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 }
-
-define('URL_PUBLIC_FOLDER', '.');
-define('URL_PROTOCOL', '//');
-define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
-define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '' , dirname($_SERVER['SCRIPT_NAME'])));
-define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'sdickerson.ddns.net');
