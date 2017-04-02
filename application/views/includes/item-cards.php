df3275fe024fd9d324e66528214524811cf1e075
Unknown <moriahmaney@gmail.com>
2017-04-01 23:37:32 -0400

    added item upload functionality and added link to item detail from img

--- application/views/includes/item-cards.php
+++ application/views/includes/item-cards.php
@@ -2,7 +2,7 @@
 foreach($items as $item)  :?>
 <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
-        <a href="#"><img class="card-img-top img-fluid" src="http://placehold.it/700x400" alt=""></a>
+        <a href="/items/item/<?php echo $item->item_id?>" class="img-container-card"><img class="card-img-top img-fluid" src="<?php if(file_exists('uploads/item_'.$item->item_id)) {echo '/uploads/item_'.$item->item_id;} else echo 'https://placehold.it/700x400';?>" alt=""></a>
         <div class="card-block">
             <small>
                     <?php
@@ -13,7 +13,7 @@ foreach($items as $item)  :?>
                         $a_cat = preg_replace('/\s+/', '', $item->category);
                         $a_subcat = preg_replace('/\s+/', '', $item->subcategory);
                         echo '<a href="/items/' . $a_cat . '">' .  $item->category . '</a>';
-                        if (isset($item->subcategory)) { 
+                        if (isset($item->subcategory)) {
                             echo ' / <a href="/items/' . $a_cat . '/' . $a_subcat . '">' . $item->subcategory . '</a>';
                         } 
                     ?>
