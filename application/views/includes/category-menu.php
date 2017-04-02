a05cb26b4aa86a6d9a4f4ac01c0f64796241c7fd
Areax <actuallythisisspam@gmail.com>
2017-04-02 19:45:21 -0400

    added an all functionality

--- application/views/includes/category-menu.php
+++ application/views/includes/category-menu.php
@@ -3,6 +3,7 @@
 
     </div>
     <div class="list-group category">
+    	<a href="/" class="list-group-item category-item">All</a>
         <a href="/items/shirts" class="list-group-item category-item">Shirts</a>
         <a href="/items/pants" class="list-group-item category-item">Pants</a>
         <a href="/items/shoes" class="list-group-item category-item">Shoes</a>
