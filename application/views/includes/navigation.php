2d69035651c6e8c571c541bd0ef1e27a142b17ca
Avi Rosenblum <nightremor@gmail.com>
2017-04-04 04:32:57 -0400

    navigation bar lights up when on that page

--- application/views/includes/navigation.php
+++ application/views/includes/navigation.php
@@ -10,17 +10,29 @@ page (which is currently a clone of the home page).
     <div class="container">
         <a class="navbar-brand" href="/">STICKERSHOCK</a>
         <div class="collapse navbar-collapse" id="navbarExample">
+        	
+        	<?php
+        		if (strpos ($_SERVER['REQUEST_URI'], 'sell'))
+        			$page=1;
+        		else if (strpos ($_SERVER['REQUEST_URI'], 'account'))
+        			$page=2;
+        		else if (strpos ($_SERVER['REQUEST_URI'], 'contact'))
+        			$page=3;
+        		else
+        			$page=0;
+        	?>
+        	
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
+                <li class= "<?php if ($page==0) echo 'nav-item active'; else echo 'nav-item'?>" >
                     <a class="nav-link" href="/">Shop<span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
+                <li class= "<?php if ($page==1) echo 'nav-item active'; else echo 'nav-item'?>">
                     <a class="nav-link" href="/pages/sell">Sell</a>
                 </li>
-                <li class="nav-item">
+                <li class= "<?php if ($page==2) echo 'nav-item active'; else echo 'nav-item'?>">
                     <a class="nav-link" href="/account">Account</a>
                 </li>
-                <li class="nav-item">
+                <li class= "<?php if ($page==3) echo 'nav-item active'; else echo 'nav-item'?>">
                     <a class="nav-link" href="/pages/contact">Contact Us</a>
                 </li>
             </ul>
