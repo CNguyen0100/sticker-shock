cdc79ff3e2c412107191e2b4e11600782cf23a26
Unknown <moriahmaney@gmail.com>
2017-04-13 13:40:05 -0400

    IN PROGRESS - paypal stuff

--- application/views/pages/success.php
+++ application/views/pages/success.php
@@ -1,6 +1,6 @@
 <?php require 'application/views/layouts/header.php';  ?>
 	<br>
     <div class="container">
-        <div class="h1">Thank You!<hr></div>
+        <div class="h1">Thank You!<hr> <?php echo var_dump($_SESSION['payment']);?></div>
     </div>
 <?php require 'application/views/layouts/footer.php'; ?>
