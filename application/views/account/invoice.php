107bcb9c95b8e476c89941433e376104eba19298
Unknown <moriahmaney@gmail.com>
2017-04-14 17:55:02 -0400

    added title to profile page & fixed session variable in invoice

--- application/views/account/invoice.php
+++ application/views/account/invoice.php
@@ -4,20 +4,20 @@ require 'application/views/layouts/header.php'; ?>
 
 <div class="container">
         <h3>Invoice:</h3>
-             <p class="media-heading">Item Name: &emsp;<?php echo $_SESSION['invoice']->item_name ?></p>
+             <p class="media-heading">Item Name: &emsp;<?php echo $invoice->item_name ?></p>
 
-             <p class="media-heading">Size: &emsp;<?php echo $_SESSION['invoice']->size ?></p>
+             <p class="media-heading">Size: &emsp;<?php echo $invoice->size ?></p>
 
-             <p class="media-heading">Shipping address: &emsp;<?php echo $_SESSION['invoice']->address_1 ?>,&emsp;<?php echo $_SESSION['invoice']->city ?>,&emsp;
-                    <?php echo $_SESSION['invoice']->state ?>,&emsp;<?php echo $_SESSION['invoice']->zip ?></p>
+             <p class="media-heading">Shipping address: &emsp;<?php echo $invoice->address_1 ?>,&emsp;<?php echo $invoice->city ?>,&emsp;
+                    <?php echo $invoice->state ?>,&emsp;<?php echo $invoice->zip ?></p>
 
-             <p class="media-heading">Completion Date: &emsp;<?php echo $_SESSION['invoice']->completion_date ?></p>
+             <p class="media-heading">Completion Date: &emsp;<?php echo $invoice->completion_date ?></p>
 
-             <p class="media-heading">Price: &emsp;$<?php echo $_SESSION['invoice']->price ?></p>
+             <p class="media-heading">Price: &emsp;$<?php echo $invoice->price ?></p>
 
-             <p class="media-heading">Shipping: &emsp;$<?php echo $_SESSION['invoice']->shipping ?></p>
+             <p class="media-heading">Shipping: &emsp;$<?php echo $invoice->shipping ?></p>
 
-             <p class="media-heading">Total: &emsp;$<?php echo $_SESSION['invoice']->total ?></p>
+             <p class="media-heading">Total: &emsp;$<?php echo $invoice->total ?></p>
 		</div>
 </div>
 
