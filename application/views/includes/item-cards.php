cb3fba90fffd1a54e0b10330cbcfe6274d0e3d87
Unknown <moriahmaney@gmail.com>
2017-04-14 22:41:10 -0400

    temp fix for word wrapping

--- application/views/includes/item-cards.php
+++ application/views/includes/item-cards.php
@@ -5,7 +5,7 @@ foreach($items as $item): ?>
 <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
         <a href="/items/item/<?php echo $item->item_id?>" class="img-container-card"><img class="card-img-top img-fluid" src="<?php if(file_exists('uploads/item_'.$item->item_id)) {echo '/uploads/item_'.$item->item_id;} else echo 'https://placehold.it/700x400';?>" alt=""></a>
-        <div class="card-block">
+        <div class="card-block" style="word-wrap: break-word;">
             <small>
                     <?php
                         # Graham L.:
@@ -21,7 +21,7 @@ foreach($items as $item): ?>
                     ?>
             </small>
             <h4 class="card-title"><a href="/items/item/<?php echo $item->item_id?>"><?php echo $item->item_name?></a></h4>
-            <h5>$<?php
+            <h5 class="card-text">$<?php
                 $total = (float)$item->price + (float)$item->shipping;
                 echo number_format((float)$total, 2, '.', '');?></h5>
             <p class="card-text"><?php echo $item->description?></p>
